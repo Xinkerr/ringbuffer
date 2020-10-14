69838c86005588b15f0cf5ecc4ca101968246fcf
郑訫 <50974571+Xinkerr@users.noreply.github.com>
2020-10-14 21:44:05 +0800

    Add files via upload

--- ringbuffer.c
+++ ringbuffer.c
@@ -1,3 +1,42 @@
+/***************************************************************************
+*
+* Copyright (c) 2019 - 2020, Xinkerr
+*
+* This file is part of ringbuffer.
+*
+* this is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Disclaimer:
+* AUTHOR MAKES NO WARRANTY, EXPRESS OR IMPLIED, ARISING BY LAW OR OTHERWISE,
+* REGARDING THE SOFTWARE (INCLUDING ANY ACOOMPANYING WRITTEN MATERIALS),
+* ITS PERFORMANCE OR SUITABILITY FOR YOUR INTENDED USE, INCLUDING,
+* WITHOUT LIMITATION, THE IMPLIED WARRANTY OF MERCHANTABILITY, THE IMPLIED
+* WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE OR USE, AND THE IMPLIED
+* WARRANTY OF NONINFRINGEMENT.
+* AUTHOR SHALL HAVE NO LIABILITY (WHETHER IN CONTRACT, WARRANTY, TORT,
+* NEGLIGENCE OR OTHERWISE) FOR ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT
+* LIMITATION, DAMAGES FOR LOSS OF BUSINESS PROFITS, BUSINESS INTERRUPTION,
+* LOSS OF BUSINESS INFORMATION, OR OTHER PECUNIARY LOSS) ARISING FROM USE OR
+* INABILITY TO USE THE SOFTWARE, INCLUDING, WITHOUT LIMITATION, ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES OR LOSS OF DATA,
+* SAVINGS OR PROFITS,
+* EVEN IF Disclaimer HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+* YOU ASSUME ALL RESPONSIBILITIES FOR SELECTION OF THE SOFTWARE TO ACHIEVE YOUR
+* INTENDED RESULTS, AND FOR THE INSTALLATION OF, USE OF, AND RESULTS OBTAINED
+* FROM, THE SOFTWARE.
+*
+* LICENSE: LGPL V2.1
+* see: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+*
+* Date: 2020/10/14
+* Version:2.0
+* Github: https://github.com/Xinkerr
+* Mail: 634326056@qq.com
+*
+****************************************************************************/
 #include <string.h>
 #include "ringbuffer.h"
 
@@ -255,4 +294,83 @@ void ringbuffer_all_clear(ringbuffer_t* rb)
 	rb->write_index = 0;
 	rb->read_index = 0;
 	rb->_bMirror = 0;
+}
+
+
+//---------------------------------新增-----------------------------------//
+
+/**@brief     从ring buffer中读出数据，非取出
+ *
+ *
+ * @param[in] rb:    ring buffer结构体
+ * @param[in] pdata: 存放数据的地址
+ * @param[in] len:   数据的长度 (byte)
+ *
+ * @return    -1：失败
+ *			   0：成功
+ */
+int8_t ringbuffer_read(ringbuffer_t* rb, uint8_t *pdata, ring_t len)
+{
+	ring_t i;
+	ring_t use_capacity;
+    ring_t tmp_read_index = rb->read_index;
+    
+	use_capacity = ringbuffer_use_space(rb);
+	#if 	DEBUG_LOG
+	printf("use:%u\n", use_capacity);
+	#endif
+	if(use_capacity >= len)
+	{
+		//数据较少时,使用这种方法
+		for(i = 0; i < len; i++)
+		{
+			pdata[i] = rb->buffer[tmp_read_index];
+
+			if(tmp_read_index == rb->buffer_size - 1)
+			{
+				rb->_bMirror = !rb->_bMirror;
+				tmp_read_index = 0;
+			}
+			else
+				tmp_read_index ++;
+		}
+		#if 	DEBUG_LOG
+		printf("read_index:%u\n", tmp_read_index);
+		#endif
+		return 0;
+	}
+	else
+		return -1;
+}
+
+/**@brief     read_index向下移动位置
+ *
+ *
+ * @param[in] rb:    ring buffer结构体
+ * @param[in] move:  read_index移动字节数
+ *
+ * @return    -1：失败
+ *			   0：成功
+ */
+int8_t ringbuffer_moveDown(ringbuffer_t* rb, ring_t move)
+{
+	ring_t use_capacity;
+    uint16_t tmp;
+    
+	use_capacity = ringbuffer_use_space(rb);
+    if(use_capacity >= move)
+    {
+        tmp = (uint16_t)rb->read_index + move;
+        if(tmp >= rb->buffer_size - 1)
+        {
+            rb->_bMirror = !rb->_bMirror;
+            rb->read_index = move - (rb->buffer_size - rb->read_index);
+        }
+        else
+        {
+            rb->read_index = tmp;
+        }
+        return 0;
+    }
+    return -1;
 }
